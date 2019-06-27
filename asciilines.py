2f8ce8aaa9efa5ef5498f4df16f5c92cc09c4d59
Michelle Duer <michelle.duer@gmail.com>
2019-06-26 18:54:24 -0700

    generate ascii canvas, compare with expected output

--- asciilines.py
+++ asciilines.py
@@ -51,38 +51,72 @@ def print_ascii_canvas(canvas: [[str]]):
         print("".join(row))
 
 
-def draw_ascii_lines(canvas: [[str]], cmd: str):
+def draw_ascii_lines(canvas: [[str]], cmd: str) -> [[str]]:
     """
     runs the command to draw ascii lines on the canvas
     :param canvas: list of each row in the canvas
     :param cmd: ascii command for drawing line
     """
-    canvas[2][1] = "&"
+    symbol, direction = cmd[0], cmd[3]
+    row = ord(cmd[1]) - 48
+    col = ord(cmd[2]) - 48
+    length = ord(cmd[4]) - 48
+
+    start_coordinates = (row, col)
+
+    # TODO: need to handle negative values for coordinates
+
+    if direction == "v":
+        end_coordinates = (row + length, col)
+        for point in range(length):
+            canvas[row + point][col] = symbol
+    else:
+        end_coordinates = (row, col + length)
+        for point in range(length):
+            canvas[row][col + point] = symbol
+
     return canvas
+
+
+def canvas_test(filename: str, canvas: [[str]]) -> bool:
+    """
+    compares canvas with expected result
+    :param canvas: list of each row in the canvas
+    :param filename: the filename that contains the canvas specifications
+    """
+    testfile = filename[:-3] + 'out'
+
+    with open(testfile, 'r') as file:
+        test_canvas = file.read().splitlines()
+
+    test_canvas = ["".join(row) for row in test_canvas]
+    test_canvas = "\n".join(test_canvas)
+
+    print(f'test canvas: \n{test_canvas}')  # TODO: delete test print
+
+    canvas = ["".join(row) for row in canvas]
+    canvas = "\n".join(canvas)
+
+    print(f'canvas: \n{canvas}')  # TODO: delete test print
+    return canvas == test_canvas
 
 
 def main(filename: str):
     """ 
-    main function that processes the specifications in the file to print an ascii canvas 
+    main function that processes the specifications in the given file
+    and uses the specs to print an ascii canvas 
     :param filename: the filename that contains the canvas specifications
     """
     dimensions, ascii_commands = define_specs(filename)
-    print(f'dimensions: {dimensions}')
-    print(f'ascii cmds:')
-    for cmd in ascii_commands:
-        print(f'{cmd}')
-
     canvas = init_canvas(dimensions)
-    print(f'init canvas:')
-    print_ascii_canvas(canvas)  # TODO test print delete
 
-    # TODO: function that runs commands (e.g. run through list of commands until end of list)
-    canvas = draw_ascii_lines(canvas, ascii_commands[0])
-    print(f'modified canvas:')
-    print_ascii_canvas(canvas)  # TODO test print delete
+    for cmd in ascii_commands:
+        canvas = draw_ascii_lines(canvas, cmd.split(" "))
 
-    # TODO: print resulting canvas
-    # TODO: compare the result with the .out file result (manual diff or function)
+    if not canvas_test(filename, canvas):
+        raise ValueError("the canvas does not contain the correct ascii values")
+
+    print_ascii_canvas(canvas)
 
 
 def parse_args():
